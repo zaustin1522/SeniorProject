0bfe682b10c10328613a9dd83ce6bfb6b3d96875
BlazekB <81893937+BlazekB@users.noreply.github.com>
2022-10-31 09:44:49 -0400

    big

--- sharify/models.py
+++ sharify/models.py
@@ -26,3 +26,17 @@ class Musicdata(models.Model):
     duration_ms  = models.IntegerField()
 
 
+class Playlist(models.Model):
+    playlist_id = models.CharField(max_length=120, primary_key=True)
+    playlist_name = models.TextField(max_length=100)
+    playlist_url = models.CharField(max_length=1000)
+    playlist_num_tracks = models.IntegerField(null=True)
+    playlist_featured = models.BooleanField(default=False)
+    playlist_genre = models.CharField(max_length=100)
+    playlist_owner = models.CharField(max_length=500)
+    date_created = models.CharField(max_length=500, default="No date")
+    playlist_img = models.ImageField(blank=True, default="")
+    songs = models.OneToMany(Musicdata)
+
+    def __str__(self):
+        return self.playlist_name
