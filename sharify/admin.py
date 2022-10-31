30236da53ddd7c22ce64e0b8903e4232bbde76f9
Jacob Dorman <dorman87@students.rowan.edu>
2022-10-31 09:02:58 -0400

    Revert "Update admin.py"
    This reverts commit 857c2d2b63258756c21d754f4991db8e71751955.

--- sharify/admin.py
+++ sharify/admin.py
@@ -4,37 +4,14 @@ from .models import Musicdata, User
 from .forms import *
 
 class Admin(UserAdmin):
-    # The forms to add and change user instances
-    form = PasswordChangeForm
     add_form = SignupForm
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ('email', 'user_dob', 'is_admin')
-    list_filter = ('is_admin',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('user_dob',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
+    form = PasswordChangeForm
+    model = User
+    list_display = ["username", "password",]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('some_extra_data',)}),
     )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'user_dob', 'password1', 'password2'),
-        }),
-    )
-    search_fields = ('email',)
-    ordering = ('email',)
-    filter_horizontal = ()
-
 
 # Register your models here.
 admin.site.register(Musicdata)
 admin.site.register(User, UserAdmin)
-
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
-admin.site.unregister(Group)
