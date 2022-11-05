556e3e1bf66a7e8d8805b867cec9b45381af4c5f
Jacob Dorman <dorman87@students.rowan.edu>
2022-11-05 16:27:27 -0400

    Update views.py

--- sharify/views.py
+++ sharify/views.py
@@ -195,7 +195,7 @@ def show_myprofile(request):
                 'current_track_name': current_track_name,
                 'current_track_artist': current_track_artist
             })
-    return render(request, 'userprofile.html', {})
+    return render(request, 'userprofile.html', {'needs_linking': True})
 
 #-----------------------------------------------------------------------------------------#
 class SignUpView(generic.CreateView):
