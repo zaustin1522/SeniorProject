889740cb4a87983b15a4730c63d2a37f9d53ded7
Jacob Dorman <dorman87@students.rowan.edu>
2022-11-06 09:41:09 -0500

    Update views.py

--- sharify/views.py
+++ sharify/views.py
@@ -218,7 +218,7 @@ def show_profile_for(request, current_user):
             return render(request, 'userprofile.html', {
             'user': current_user,
             'needs_linking': True,
-            'message': response
+            'message': response.content
             })
         return render(request, 'userprofile.html', {
             'user': current_user,
