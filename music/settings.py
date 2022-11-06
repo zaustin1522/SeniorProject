90eb709831c626acec2735f9e98cc754a570f143
Jacob Dorman <dorman87@students.rowan.edu>
2022-11-06 11:52:17 -0500

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -53,6 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'social_django.middleware.SocialAuthExceptionMiddleware',
+    ]
+
 ROOT_URLCONF = 'music.urls'
 
 TEMPLATES = [
@@ -66,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
